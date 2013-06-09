7c6c18759a33f20563ffeaa9add1c8c59c4dcdf1
Peter White <pwhite2@gmail.com>
2013-06-09 15:31:47 -0700

    Adding additional content for the demo.

--- app/assets/javascripts/semantic_analysis.js
+++ app/assets/javascripts/semantic_analysis.js
@@ -5,6 +5,10 @@ var SemanticAnalysis = function(){
 			'Consider <strong>alerting the authorities</strong> or <strong>emailing</strong> us <strong>report@saviour.herokuapp.com</strong>) immediately.',
 			'Is this person going to harm someone? Help them <strong>consider the consequences</strong>. Ask them if they have <strong>family</strong>'
 			],
+		'maim' : [
+			'Consider <strong>alerting the authorities</strong> or <strong>emailing</strong> us <strong>report@saviour.herokuapp.com</strong>) immediately.',
+			'Is this person going to harm someone? Help them <strong>consider the consequences</strong>. Ask them if they have <strong>family</strong>'
+			],
 		'suicide' : [
 			'Ask them how their death would <strong>affect their family<strong>',
 			'Think about his <strong>children</strong> (if he has any)',
@@ -13,8 +17,11 @@ var SemanticAnalysis = function(){
 		'died' : [
 			'Tell them about a hardship of yours and how you <strong>overcame</strong> a similar situation',
 			'Consider having a philosophial discussion about the meaning of life'
-		]
-	}
+		],
+		'die' : [
+			'Tell them about a hardship of yours and how you <strong>overcame</strong> a similar situation',
+			'Consider having a philosophial discussion about the meaning of life'
+		]	}
 
 	var analyze = function(text){
 		var matches = [];
